d69bc3c2b7dc3ef0123737649eafeb8681690f20
Josh Maggard <jmaggard62@gmail.com>
2024-04-14 12:02:44 -0500

    starting world

--- PolygonWithCollision.gd
+++ PolygonWithCollision.gd
@@ -4,3 +4,5 @@ func _ready() -> void:
 	var collision_poly = CollisionPolygon2D.new()
 	get_parent().add_child.call_deferred(collision_poly)
 	collision_poly.polygon = polygon
+	collision_poly.position = position
+
